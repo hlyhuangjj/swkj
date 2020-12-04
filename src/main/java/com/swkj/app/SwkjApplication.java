c7ffe5c9cff661b6e9635f79c46d4538707644e8
hlyhuangjj <250151429@qq.com>
2020-12-04 09:18:44 +0800

    Initial commit

---
+++ src/main/java/com/swkj/app/SwkjApplication.java
@@ -0,0 +1,13 @@
+package com.swkj.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwkjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwkjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swkj/app/SwkjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swkj.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwkjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
